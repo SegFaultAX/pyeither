e042fba56f1cbb1475b186b7cf0cbba27fab6f6b
Michael-Keith Bernard <mkbernard@opentable.com>
2018-01-11 17:51:31 -0800

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(
-    name='pyeither',
-    version='0.0.1',
-    packages=['either',],
-    license='MIT',
-    description='An implementation of Data.Either from Haskell in Python',
-    long_description=open('README.md').read(),
-    url='https://github.com/segfaultax/pyeither',
-    author='Michael-Keith Bernard',
-    author_email='mkbernard.dev@gmail.com',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ],
-    keywords='either haskell data.either monad applicative functor',
-    install_requires=[
-        'attrs'
-    ],
-)
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='pyeither',
+        version='0.0.1',
+        packages=['either',],
+        license='MIT',
+        description='An implementation of Data.Either from Haskell in Python',
+        long_description=open('README.md').read(),
+        url='https://github.com/segfaultax/pyeither',
+        author='Michael-Keith Bernard',
+        author_email='mkbernard.dev@gmail.com',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+        ],
+        keywords='either haskell data.either monad applicative functor',
+        install_requires=[
+            'attrs'
+        ],
+    )
