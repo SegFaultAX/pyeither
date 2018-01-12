556bdd5a9cc8cc378bba68fe38e53a5b269a793a
Michael-Keith Bernard <mkbernard@opentable.com>
2018-01-11 19:12:22 -0800

    Fix Python 3 compat

--- either/__init__.py
+++ either/__init__.py
@@ -23,6 +23,13 @@
 
 """An implementation of `data Either e a = Left e | Right a` in Python"""
 
+import sys
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    from functools import reduce
+
 import attr
 
 __author__ = "Michael-Keith Bernard"
@@ -337,85 +344,85 @@ if __name__ == "__main__":
 
     class EitherTest(unittest.TestCase):
         def test_left(self):
-            self.assertEquals(Left("foo").get(), "foo")
-            self.assertEquals(left("foo").get(), "foo")
+            self.assertEqual(Left("foo").get(), "foo")
+            self.assertEqual(left("foo").get(), "foo")
 
         def test_right(self):
-            self.assertEquals(Right("foo").get(), "foo")
-            self.assertEquals(right("foo").get(), "foo")
+            self.assertEqual(Right("foo").get(), "foo")
+            self.assertEqual(right("foo").get(), "foo")
 
         def test_fmap(self):
-            self.assertEquals(fmap(lambda e: e + 1, right(1)), right(2))
-            self.assertEquals(fmap(lambda e: e + 1, left("err")), left("err"))
+            self.assertEqual(fmap(lambda e: e + 1, right(1)), right(2))
+            self.assertEqual(fmap(lambda e: e + 1, left("err")), left("err"))
 
         def test_app(self):
-            self.assertEquals(app(right(lambda e: e + 1), right(1)), right(2))
-            self.assertEquals(app(right(lambda e: e + 1), left("err")), left("err"))
-            self.assertEquals(app(left("err1"), left("err2")), left("err1"))
+            self.assertEqual(app(right(lambda e: e + 1), right(1)), right(2))
+            self.assertEqual(app(right(lambda e: e + 1), left("err")), left("err"))
+            self.assertEqual(app(left("err1"), left("err2")), left("err1"))
 
         def test_pure(self):
-            self.assertEquals(pure(1), right(1))
+            self.assertEqual(pure(1), right(1))
 
         def test_bind(self):
-            self.assertEquals(bind(pure(1), lambda e: right(e + 1)), right(2))
-            self.assertEquals(bind(left("err"), lambda e: right(e + 1)), left("err"))
+            self.assertEqual(bind(pure(1), lambda e: right(e + 1)), right(2))
+            self.assertEqual(bind(left("err"), lambda e: right(e + 1)), left("err"))
 
         def test_combine(self):
-            self.assertEquals(combine(pure(1), pure(2)), pure(1))
-            self.assertEquals(combine(left("err"), pure(2)), pure(2))
-            self.assertEquals(combine(left("err"), left("err!")), left("err!"))
+            self.assertEqual(combine(pure(1), pure(2)), pure(1))
+            self.assertEqual(combine(left("err"), pure(2)), pure(2))
+            self.assertEqual(combine(left("err"), left("err!")), left("err!"))
 
         def test_bimap(self):
             lf = lambda e: e + "!"
             rf = lambda e: e + 1
-            self.assertEquals(bimap(lf, rf, right(1)), right(2))
-            self.assertEquals(bimap(lf, rf, left("err")), left("err!"))
+            self.assertEqual(bimap(lf, rf, right(1)), right(2))
+            self.assertEqual(bimap(lf, rf, left("err")), left("err!"))
 
         def test_foldr(self):
-            self.assertEquals(foldr(lambda e, v: e / v, 2, pure(10)), 5)
-            self.assertEquals(foldr(lambda e, v: e / v, 2, left("err")), 2)
+            self.assertEqual(foldr(lambda e, v: e / v, 2, pure(10)), 5)
+            self.assertEqual(foldr(lambda e, v: e / v, 2, left("err")), 2)
 
         def test_traverse(self):
-            self.assertEquals(traverse(lambda e: Id(e * 2), pure(5), Id), Id(pure(10)))
-            self.assertEquals(traverse(lambda e: Id(e * 2), left("err"), Id), Id(left("err")))
+            self.assertEqual(traverse(lambda e: Id(e * 2), pure(5), Id), Id(pure(10)))
+            self.assertEqual(traverse(lambda e: Id(e * 2), left("err"), Id), Id(left("err")))
 
         def test_sequence(self):
-            self.assertEquals(sequence(pure(Id(1)), Id), Id(pure(1)))
-            self.assertEquals(sequence(left("err"), Id), Id(left("err")))
+            self.assertEqual(sequence(pure(Id(1)), Id), Id(pure(1)))
+            self.assertEqual(sequence(left("err"), Id), Id(left("err")))
 
         def test_join(self):
-            self.assertEquals(join(pure(pure(5))), pure(5))
+            self.assertEqual(join(pure(pure(5))), pure(5))
 
         def test_lmap(self):
-            self.assertEquals(lmap(lambda e: e + "!", right(1)), right(1))
-            self.assertEquals(lmap(lambda e: e + "!", left("err")), left("err!"))
+            self.assertEqual(lmap(lambda e: e + "!", right(1)), right(1))
+            self.assertEqual(lmap(lambda e: e + "!", left("err")), left("err!"))
 
         def test_attempt(self):
             def angry():
                 raise RuntimeError("so angry")
             self.assertTrue(attempt(angry).is_left)
-            self.assertEquals(attempt(angry).get().message, "so angry")
-            self.assertEquals(attempt(lambda e: e, 5), pure(5))
+            self.assertTrue(isinstance(attempt(angry).get(), RuntimeError))
+            self.assertEqual(attempt(lambda e: e, 5), pure(5))
 
         def test_should(self):
-            self.assertEquals(should(True, 1, "err"), pure(1))
-            self.assertEquals(should(False, 1, "err"), left("err"))
+            self.assertEqual(should(True, 1, "err"), pure(1))
+            self.assertEqual(should(False, 1, "err"), left("err"))
 
         def test_predicate(self):
-            self.assertEquals(predicate(lambda e: True, "err")(1), pure(1))
-            self.assertEquals(predicate(lambda e: False, "err")(1), left("err"))
+            self.assertEqual(predicate(lambda e: True, "err")(1), pure(1))
+            self.assertEqual(predicate(lambda e: False, "err")(1), left("err"))
 
         def test_chain(self):
             c1 = chain(pure(1)).fmap(lambda e: e + 1).bind(lambda e: pure(e * 2))
-            self.assertEquals(c1.unchain(), right(4))
+            self.assertEqual(c1.unchain(), right(4))
 
             c2 = chain(pure(1)).bind(lambda _: left("err")).lmap(lambda e: e + "!")
-            self.assertEquals(c2.unchain(), left("err!"))
+            self.assertEqual(c2.unchain(), left("err!"))
 
             lf = lambda e: e + "!"
             rf = lambda e: e * 10
             c3 = chain(pure(lambda e: e + 1)).app(pure(1)).bimap(lf, rf)
-            self.assertEquals(c3.unchain(), right(20))
+            self.assertEqual(c3.unchain(), right(20))
 
         def test_kleisli(self):
             f1 = lambda e: pure(int(e))
@@ -424,8 +431,8 @@ if __name__ == "__main__":
             f4 = lambda e: left("err")
             f5 = kleisli(f3, f4)
 
-            self.assertEquals(bind(pure("10"), f3), right(100))
-            self.assertEquals(bind(pure("10"), f5), left("err"))
+            self.assertEqual(bind(pure("10"), f3), right(100))
+            self.assertEqual(bind(pure("10"), f5), left("err"))
 
     class Laws(unittest.TestCase):
         def test_functor(self):
@@ -434,10 +441,10 @@ if __name__ == "__main__":
             g_of_f = lambda e: g(f(e))
 
             # fmap id x = x                  -- Identity
-            self.assertEquals(fmap(identity, pure(5)), pure(5))
+            self.assertEqual(fmap(identity, pure(5)), pure(5))
 
             # fmap (g . f) = fmap g . fmap f -- Composition
-            self.assertEquals(fmap(g_of_f, pure(5)), fmap(g, fmap(f, pure(5))))
+            self.assertEqual(fmap(g_of_f, pure(5)), fmap(g, fmap(f, pure(5))))
 
         def test_applicative(self):
             sup = lambda x: lambda f: f(x)
@@ -446,30 +453,30 @@ if __name__ == "__main__":
             v = lambda e: e * 10
 
             # pure id <*> v = v                            -- Identity
-            self.assertEquals(app(pure(identity), pure(1)), pure(1))
+            self.assertEqual(app(pure(identity), pure(1)), pure(1))
 
             # pure f <*> pure x = pure (f x)               -- Homomorphism
-            self.assertEquals(app(pure(lambda e: e + 1), pure(1)), pure(2))
+            self.assertEqual(app(pure(lambda e: e + 1), pure(1)), pure(2))
 
             # u <*> pure y = pure ($ y) <*> u              -- Interchange
-            self.assertEquals(
+            self.assertEqual(
                 app(pure(lambda e: e + "!"), pure("hello")),
                 app(pure(sup("hello")), pure(lambda e: e + "!")))
 
             # pure (.) <*> u <*> v <*> w = u <*> (v <*> w) -- Composition
-            self.assertEquals(
+            self.assertEqual(
                 app(app(app(pure(comp2), pure(u)), pure(v)), pure(5)),
                 app(pure(u), app(pure(v), pure(5))))
 
         def test_monad(self):
             # m >>= return     =  m                        -- Right Unit
-            self.assertEquals(bind(pure(5), pure), pure(5))
+            self.assertEqual(bind(pure(5), pure), pure(5))
 
             # return x >>= f   =  f x                      -- Left Unit
-            self.assertEquals(bind(pure(5), lambda e: pure(e + 5)), pure(10))
+            self.assertEqual(bind(pure(5), lambda e: pure(e + 5)), pure(10))
 
             # (m >>= f) >>= g  =  m >>= (\x -> f x >>= g)  -- Associativity
-            self.assertEquals(
+            self.assertEqual(
                 bind(bind(pure("5"), lambda e: pure(int(e))), lambda e: e + 5),
                 bind(pure("5"), lambda e: bind(pure(int(e)), lambda e: e + 5)))
 
