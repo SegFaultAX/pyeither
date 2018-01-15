0de57b3005c95cbe99a9e3e43ea50df85de230d4
Michael-Keith Bernard <mkbernard@opentable.com>
2018-01-14 23:27:18 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='pyeither',
-        version='0.0.2',
+        version='0.0.3',
         packages=['either',],
         license='MIT',
         description='An implementation of Data.Either from Haskell in Python',
